d20bcf555dd368e67eef401246e63b7067c2854f
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-08-24 14:26:44 +0100

    Add initial getter trait and NI

---
+++ src/getter.rs
@@ -0,0 +1,13 @@
+use async_trait::async_trait;
+use geojson::FeatureCollection;
+use polars::prelude::DataFrame;
+
+/// Gets population and GIS data implemented on a given country.
+// TODO: refine methods and types
+#[async_trait]
+pub trait Getter {
+    /// Gets population data.
+    async fn population(&self) -> anyhow::Result<DataFrame>;
+    /// Gets GeoJSON data.
+    async fn geojson(&self) -> anyhow::Result<FeatureCollection>;
+}
