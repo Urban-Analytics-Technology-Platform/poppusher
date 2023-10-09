057623f3ad8d854e552db67304cf4cf5c06de2d9
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-10-09 09:58:35 +0100

    Fix doc comment

--- src/getter.rs
+++ src/getter.rs
@@ -10,6 +10,6 @@ pub trait Getter {
     async fn population(&self) -> anyhow::Result<DataFrame>;
     /// Gets GeoJSON data.
     async fn geojson(&self) -> anyhow::Result<FeatureCollection>;
-    /// Gets GeoJSON data.
+    /// Gets GeoJSON data as a dataframe.
     async fn geojson_dataframe(&self) -> anyhow::Result<DataFrame>;
 }
