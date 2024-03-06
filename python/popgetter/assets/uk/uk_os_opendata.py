492fe3a1bf61135052da6e7fb0c25bda01391473
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2024-03-06 11:56:04 +0000

    lists main URLs for OS OpenData

---
+++ python/popgetter/assets/uk/uk_os_opendata.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+all_datasets = [
+    {
+        "name": "1:250 000 Scale Colour Raster",
+        "download_url": "https://api.os.uk/downloads/v1/products/250kScaleColourRaster/downloads?area=GB&format=TIFF-LZW&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/250kScaleColourRaster",
+    },
+    {
+        "name": "Boundary-Line",
+        "download_url": "https://api.os.uk/downloads/v1/products/BoundaryLine/downloads?area=GB&format=GeoPackage&redirect",
+        # "https://api.os.uk/downloads/v1/products/BoundaryLine/downloads?area=GB&format=Vector+Tiles&subformat=%28MBTiles%29&redirect"
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/BoundaryLine",
+    },
+    {
+        "name": "Code-Point® Open",
+        "download_url": "https://api.os.uk/downloads/v1/products/CodePointOpen/downloads?area=GB&format=GeoPackage&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/CodePointOpen",
+    },
+    {
+        "name": "GB Overview Maps",
+        "download_url": "https://api.os.uk/downloads/v1/products/GBOverviewMaps/downloads?area=GB&format=GeoTIFF&subformat=Full+Colour&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/GBOverviewMaps",
+    },
+    {
+        "name": "MiniScale®",
+        "download_url": "https://api.os.uk/downloads/v1/products/MiniScale/downloads?area=GB&format=Zip+file+%28containing+EPS%2C+Illustrator+and+TIFF-LZW%29&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/MiniScale",
+    },
+    {
+        "name": "OS Open Built Up Areas",
+        "download_url": "https://api.os.uk/downloads/v1/products/BuiltUpAreas/downloads?area=GB&format=GeoPackage&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/BuiltUpAreas",
+    },
+    {
+        "name": "OS Open Greenspace",
+        "download_url": "https://api.os.uk/downloads/v1/products/OpenGreenspace/downloads?area=GB&format=GeoPackage&redirect",
+        # "https://api.os.uk/downloads/v1/products/OpenGreenspace/downloads?area=GB&format=Vector+Tiles&subformat=%28MBTiles%29&redirect"
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/OpenGreenspace",
+    },
+    {
+        "name": "OS Open Linked Identifiers",
+        # "Many download_urlS!"
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/LIDS",
+    },
+    {
+        "name": "OS Open Names",
+        "download_url": "https://api.os.uk/downloads/v1/products/OpenNames/downloads?area=GB&format=GeoPackage&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/OpenNames",
+    },
+    {
+        "name": "OS Open Rivers",
+        "download_url": "https://api.os.uk/downloads/v1/products/OpenRivers/downloads?area=GB&format=GeoPackage&redirect",
+        # "https://api.os.uk/downloads/v1/products/OpenRivers/downloads?area=GB&format=Vector+Tiles&subformat=%28MBTiles%29&redirect"
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/OpenRivers",
+    },
+    {
+        "name": "OS Open Roads",
+        "download_url": "https://api.os.uk/downloads/v1/products/OpenRoads/downloads?area=GB&format=GeoPackage&redirect",
+        # "https://api.os.uk/downloads/v1/products/OpenRoads/downloads?area=GB&format=Vector+Tiles&subformat=%28MBTiles%29&redirect"
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/OpenRoads",
+    },
+    {
+        "name": "OS Open TOID",
+        # "Download per OSGB Grid Tile {eg 100km x 100km}"
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/OpenTOID",
+    },
+    {
+        "name": "OS Open UPRN",
+        "download_url": "https://api.os.uk/downloads/v1/products/OpenUPRN/downloads?area=GB&format=GeoPackage&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/OpenUPRN",
+    },
+    {
+        "name": "OS Open USRN",
+        "download_url": "https://api.os.uk/downloads/v1/products/OpenUSRN/downloads?area=GB&format=GeoPackage&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/OpenUSRN",
+    },
+    {
+        "name": "OS Open Zoomstack",
+        "download_url": "https://api.os.uk/downloads/v1/products/OpenZoomstack/downloads?area=GB&format=GeoPackage&redirect",
+        # "https://api.os.uk/downloads/v1/products/OpenZoomstack/downloads?area=GB&format=Vector+Tiles&subformat=%28MBTiles%29&redirect"
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/OpenZoomstack",
+    },
+    {
+        "name": "OS OpenMap - Local",
+        "download_url": "https://api.os.uk/downloads/v1/products/OpenMapLocal/downloads?area=GB&format=GeoPackage&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/OpenMapLocal",
+    },
+    {
+        "name": "OS Terrain® 50",
+        "download_url": "https://api.os.uk/downloads/v1/products/Terrain50/downloads?area=GB&format=GeoPackage&redirect",
+        # "https://api.os.uk/downloads/v1/products/Terrain50/downloads?area=GB&format=Vector+Tiles&subformat=%28MBTiles%29&redirect"
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/Terrain50",
+    },
+    {
+        "name": "OS VectorMap® District",
+        "download_url": "https://api.os.uk/downloads/v1/products/VectorMapDistrict/downloads?area=GB&format=GeoPackage&redirect",
+        "landpage_url": "https://osdatahub.os.uk/downloads/open/VectorMapDistrict",
+    },
+]
