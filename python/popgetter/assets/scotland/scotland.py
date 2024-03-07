9dadda2cca1fb21e9ea88a5e190c139ab967cb98
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-03-07 22:19:13 +0000

    Format, make partition keys unique, add geographies asset

--- python/popgetter/assets/scotland/scotland.py
+++ python/popgetter/assets/scotland/scotland.py
@@ -2,6 +2,7 @@ import subprocess
 import tempfile
 from typing import Tuple
 import requests
+
 # import zipfile
 import zipfile_deflate64 as zipfile
 import os
@@ -11,28 +12,20 @@ import geopandas
 import numpy as np
 import matplotlib.pyplot as plt
 from icecream import ic
-from dagster import AssetIn, AssetOut, DynamicPartitionsDefinition, MetadataValue, Output, SpecificPartitionsPartitionMapping, StaticPartitionsDefinition, asset, multi_asset
-
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
-}
-
-
-def download_file(
-    cache_dir: str,
-    url: str,
-    file_name: str | None = None,
-    headers: dict[str, str] = HEADERS,
-) -> str:
-    """Downloads file checking first if exists in cache, returning file name."""
-    file_name = (
-        os.path.join(cache_dir, url.split("/")[-1]) if file_name is None else file_name
-    )
-    if not os.path.exists(file_name):
-        r = requests.get(url, allow_redirects=True, headers=headers)
-        open(file_name, "wb").write(r.content)
-    return file_name
-
+import popgetter
+from dagster import (
+    AssetIn,
+    AssetKey,
+    AssetOut,
+    DynamicPartitionsDefinition,
+    MetadataValue,
+    Output,
+    SpecificPartitionsPartitionMapping,
+    StaticPartitionsDefinition,
+    asset,
+    multi_asset,
+    op,
+)
 
 """
 Notes:
@@ -64,26 +57,44 @@ GeoCodeLookup = {
     "LSOA11": 1,  # "SNS Data Zone 2011 blk"
     "OA11": 2,  # "Output Area blk"
 }
-# SCGeoCodes = ["CA", "DZ", "OA"]
-
 
 DATA_SOURCES = {
     0: {
         "source": "Council Area blk",
         "resolution": "LAD",
-        "url": URL1 + "/media/hjmd0oqr/council-area-blk.zip"
+        "url": URL1 + "/media/hjmd0oqr/council-area-blk.zip",
     },
     1: {
         "source": "SNS Data Zone 2011 blk",
         "resolution": "LSOA11",
-        "url": URL2 + urlparse.quote("SNS Data Zone 2011 blk") + ".zip"
+        "url": URL2 + urlparse.quote("SNS Data Zone 2011 blk") + ".zip",
     },
     2: {
         "source": "Output Area blk",
         "resolution": "OA11",
-        "url": URL2 + urlparse.quote("Output Area blk") + ".zip"
-    }
+        "url": URL2 + urlparse.quote("Output Area blk") + ".zip",
+    },
 }
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
+}
+
+
+def download_file(
+    cache_dir: str,
+    url: str,
+    file_name: str | None = None,
+    headers: dict[str, str] = HEADERS,
+) -> str:
+    """Downloads file checking first if exists in cache, returning file name."""
+    file_name = (
+        os.path.join(cache_dir, url.split("/")[-1]) if file_name is None else file_name
+    )
+    if not os.path.exists(file_name):
+        r = requests.get(url, allow_redirects=True, headers=headers)
+        open(file_name, "wb").write(r.content)
+    return file_name
 
 
 # NB. Make sure no spaces in asset keys
@@ -109,34 +120,39 @@ def source_to_zip(source_name: str, url: str) -> str:
     file_name = os.path.join(cache_dir, source_name.replace(" ", "_") + ".zip")
     return download_file(cache_dir, url, file_name)
 
+
 @asset
-def make_catalog(context) -> pd.DataFrame:
+def catalog(context) -> pd.DataFrame:
     records = []
     for data_source in DATA_SOURCES.values():
         resolution = data_source["resolution"]
         source = data_source["source"]
         url = data_source["url"]
-        with zipfile.ZipFile(source_to_zip(source, url)) as zip_ref: 
+        with zipfile.ZipFile(source_to_zip(source, url)) as zip_ref:
             for name in zip_ref.namelist():
                 print(name)
                 record = {
-                        "resolution": resolution,
-                        "source": source,
-                        "url": url,
-                        "file_name": name,
-                    }
+                    "resolution": resolution,
+                    "source": source,
+                    "url": url,
+                    "file_name": name,
+                }
                 records.append(record)
                 ic(record)
                 zip_ref.extract(name, cache_dir)
-    
+
+    # TODO: check if required
     for partition in context.instance.get_dynamic_partitions(PARTITIONS_DEF_NAME):
         context.instance.delete_dynamic_partition(PARTITIONS_DEF_NAME, partition)
 
     # Create a dynamic partition for the datasets listed in the catalog
     catalog_df: pd.DataFrame = pd.DataFrame.from_records(records)
-    partition_keys = catalog_df["file_name"].to_list()
+    catalog_df["partition_keys"] = (
+        catalog_df[["resolution", "file_name"]].agg("/".join, axis=1).to_list()
+    )
     context.instance.add_dynamic_partitions(
-        partitions_def_name=PARTITIONS_DEF_NAME, partition_keys=partition_keys
+        partitions_def_name=PARTITIONS_DEF_NAME,
+        partition_keys=catalog_df["partition_keys"].to_list(),
     )
     context.add_output_metadata(
         metadata={
@@ -156,9 +172,8 @@ def get_table(context, table_details) -> pd.DataFrame:
     df = pd.read_csv(os.path.join(cache_dir, table_details["file_name"].iloc[0]))
     context.add_output_metadata(
         metadata={
-            "title": table_details["file_nae"].iloc[0],
-            # "title": "Test",
-            "num_records": len(df),  # Metadata can be any key-value pair
+            "title": table_details["partition_keys"].iloc[0],
+            "num_records": len(df),
             "columns": MetadataValue.md(
                 "\n".join([f"- '`{col}`'" for col in df.columns.to_list()])
             ),
@@ -167,32 +182,58 @@ def get_table(context, table_details) -> pd.DataFrame:
     )
     return df
 
+
 @asset(partitions_def=dataset_node_partition)
-def individual_census_table(context, make_catalog: pd.DataFrame) -> pd.DataFrame:
+def individual_census_table(context, catalog: pd.DataFrame) -> pd.DataFrame:
     partition_key = context.asset_partition_key_for_output()
-    ic(partition_key)
-    row = make_catalog.loc[make_catalog["file_name"].isin([partition_key])]
-    ic(row)
+    context.log.info(partition_key)
+    row = catalog.loc[catalog["partition_keys"].isin([partition_key])]
+    context.log.info(row)
     return get_table(context, table_details=row)
 
 
-# # TODO: add to derived
-# def get_lc1117sc(context, lookup, ) -> pd.DataFrame:
+# @op
+# def lc1117sc(context, individual_census_table, oa_dz_iz_2011_lookup) -> pd.DataFrame:
 #     """Gets LC1117SC age by sex table at OA11 resolution."""
-#     df = get_rawdata("LC1117SC", "OA11").rename(
-#         columns={"Unnamed: 0": "OA11", "Unnamed: 1": "Age bracket"}
-#     )
-#     return df.loc[df["OA11"].isin(lookup["OutputArea2011Code"])]
+#     from popgetter import defs
+#     with defs.get_asset_value_loader(instance=context.instance) as loader:
+#         df = loader.load_asset_value(AssetKey(["uk-scotland",  "individual_census_table"]), partition_key="LC1117SC.csv")
+#         df = df.rename(
+#             columns={"Unnamed: 0": "OA11", "Unnamed: 1": "Age bracket"}
+#         )
+#         df = df.loc[df["OA11"].isin(oa_dz_iz_2011_lookup["OutputArea2011Code"])]
+#         context.add_output_metadata(
+#             metadata = {
+#                 "title": df["file_name"].iloc[0],
+#                 "num_records": len(df),
+#                 "columns": MetadataValue.md(
+#                     "\n".join([f"- '`{col}`'" for col in df.columns.to_list()])
+#                 ),
+#                 "preview": MetadataValue.md(df.head().to_markdown()),
+#             }
+#         )
+#         return df
 
 
-# # TODO: add shapefile
-# def shapefile(context) -> geopandas.GeoDataFrame:
-#     """Gets the shape file for OA11 resolution."""
-#     file_name = download_file(cache_dir, URL_SHAPEFILE)
-#     geo = geopandas.read_file(f"zip://{file_name}")
-#     return geo[geo["geo_code"].isin(lookup["OutputArea2011Code"])]
+@asset
+def geometry(context, oa_dz_iz_2011_lookup) -> geopandas.GeoDataFrame:
+    """Gets the shape file for OA11 resolution."""
+    file_name = download_file(cache_dir, URL_SHAPEFILE)
+    geo = geopandas.read_file(f"zip://{file_name}")
+    # TODO: add metadat for geopandas
+    # context.add_output_metadata(
+    #     metadata={
+    #         "title": table_details["partition_keys"].iloc[0],
+    #         "num_records": len(df),
+    #         "columns": MetadataValue.md(
+    #             "\n".join([f"- '`{col}`'" for col in df.columns.to_list()])
+    #         ),
+    #         "preview": MetadataValue.md(df.head().to_markdown()),
+    #     }
+    # )
+    return geo[geo["geo_code"].isin(oa_dz_iz_2011_lookup["OutputArea2011Code"])]
 
-    
+
 # # TODO: add plots
 # @asset
 # def generate_plots():
