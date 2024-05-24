0d42d27057b8074d71ee88938296886afd42decf
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-05-24 09:27:18 +0100

    Add processing for pivoting arbitrary census tables

--- python/popgetter/assets/ni/__init__.py
+++ python/popgetter/assets/ni/__init__.py
@@ -520,7 +520,7 @@ class NorthernIreland(Country, CountryAssetOuputs):
             # TODO: how programmatically do this
             hxltag="TBD",
             geo_level=geo_level,
-            geo_column=NI_GEO_LEVELS["geo_level"].geo_id_column,
+            geo_column=NI_GEO_LEVELS[geo_level].geo_id_column,
             source_column="Count",
         )
 
@@ -536,47 +536,113 @@ class NorthernIreland(Country, CountryAssetOuputs):
         census_tables: pd.DataFrame,
         source_metric_metadata: MetricMetadata,
     ) -> tuple[list[MetricMetadata], pd.DataFrame]:
+        SEP = "_"
         partition_key = context.partition_key
+        geo_level = partition_key.split("/")[0]
         source_table = census_tables
         source_mmd = source_metric_metadata
         source_column = source_mmd.parquet_column_name
         assert source_column in source_table.columns
         assert len(source_table) > 0
 
-        try:
-            source_table_metadata, metric_specs = DERIVED_COLUMN_SPECIFICATIONS[
-                partition_key
-            ]
-        except KeyError as err:
-            skip_reason = f"Skipping as no derived columns are to be created for node {partition_key}"
-            context.log.warning(skip_reason)
-            raise RuntimeError(skip_reason) from err
-
-        source_table = source_table.rename(
-            columns={source_table_metadata.geo_column: "GEO_ID"}
+        geo_id = NI_GEO_LEVELS[geo_level].census_table_column
+        source_table = source_table.rename(columns={geo_id: "GEO_ID"}).drop(
+            columns=geo_id.replace("Code", "Label")
         )
-        derived_metrics: list[pd.DataFrame] = []
-        derived_mmd: list[MetricMetadata] = []
+
         parquet_file_name = (
             "".join(c for c in partition_key if c.isalnum()) + ".parquet"
         )
-        for metric_spec in metric_specs:
-            new_table = (
-                source_table.pipe(metric_spec.filter_func)
-                .groupby(by="GEO_ID", as_index=True)
-                .sum()
-                .rename(columns={source_column: metric_spec.output_column_name})
-                .filter(items=["GEO_ID", metric_spec.output_column_name])
-            )
-            derived_metrics.append(new_table)
+        derived_metrics: list[pd.DataFrame] = []
+        derived_mmd: list[MetricMetadata] = []
 
-            new_mmd = source_mmd.copy()
-            new_mmd.parent_metric_id = source_mmd.source_metric_id
-            new_mmd.metric_parquet_path = parquet_file_name
-            new_mmd.hxl_tag = metric_spec.hxltag
-            new_mmd.parquet_column_name = metric_spec.output_column_name
-            new_mmd.human_readable_name = metric_spec.human_readable_name
-            derived_mmd.append(new_mmd)
+        try:
+            # TODO: check whether to drop unused source_table_metadata
+            source_table_metadata, metric_specs = DERIVED_COLUMN_SPECIFICATIONS[
+                partition_key
+            ]
+            for metric_spec in metric_specs:
+                new_table = (
+                    source_table.pipe(metric_spec.filter_func)
+                    .groupby(by="GEO_ID", as_index=True)
+                    .sum()
+                    .rename(columns={source_column: metric_spec.output_column_name})
+                    .filter(items=["GEO_ID", metric_spec.output_column_name])
+                )
+                derived_metrics.append(new_table)
+                new_mmd = source_mmd.copy()
+                new_mmd.parent_metric_id = source_mmd.source_metric_id
+                new_mmd.metric_parquet_path = parquet_file_name
+                new_mmd.hxl_tag = metric_spec.hxltag
+                new_mmd.parquet_column_name = metric_spec.output_column_name
+                new_mmd.human_readable_name = metric_spec.human_readable_name
+                derived_mmd.append(new_mmd)
+        except KeyError:
+            skip_reason = f"Skipping as no derived columns are to be created for node {partition_key}"
+            context.log.warning(skip_reason)
+            # raise RuntimeError(skip_reason) from err
+
+        # Get all other metrics from table as is pivoted
+        def pivot_df(df: pd.DataFrame, end: str) -> tuple[list[str], pd.DataFrame]:
+            # Variables are either code or label, only keep the case for given 'end'
+            cols = (
+                [col for col in df.columns if col.endswith(end)]
+                + ["GEO_ID"]
+                + ["Count"]
+            )
+            pivot_cols = [col for col in cols if col not in ["GEO_ID", "Count"]]
+            ic(cols)
+            ic(pivot_cols)
+            ic(df.columns)
+            ic(df.head())
+            pivot = df[cols].pivot_table(
+                index="GEO_ID",
+                columns=pivot_cols,
+                values="Count",
+            )
+
+            # FLattent multi-index
+            if isinstance(pivot.columns, pd.MultiIndex):
+                pivot.columns = [
+                    SEP.join(list(map(str, col))).strip()
+                    for col in pivot.columns.to_numpy()
+                ]
+            # Ensure columns are string
+            else:
+                pivot.columns = [str(col).strip() for col in pivot.columns.to_numpy()]
+            out_cols = [col.replace(var_type, "").strip() for col in pivot_cols]
+            return out_cols, pivot
+
+        # Pivot for codes and labels
+        for var_type in ["Code", "Label"]:
+            out_cols, new_table = pivot_df(source_table, var_type)
+            ic(new_table)
+            for metric_col in new_table.columns:
+                metric_df = new_table.loc[:, metric_col].to_frame()
+                ic(metric_df)
+                derived_metrics.append(metric_df)
+                new_mmd = source_mmd.copy()
+                new_mmd.parent_metric_id = source_mmd.source_metric_id
+                new_mmd.metric_parquet_path = parquet_file_name
+                key_val = dict(zip(out_cols, metric_col.split(SEP), strict=True))
+
+                def gen_hxltag(kv: dict[str, str]) -> str:
+                    out = ["#population"]
+                    for key, value in kv.items():
+                        out += ["".join(c for c in key if c.isalnum())]
+                        out += ["_"]
+                        out += ["".join(c for c in value if c.isalnum())]
+                    return "+".join(out)
+
+                new_mmd.hxl_tag = gen_hxltag(key_val)
+                new_mmd.parquet_column_name = metric_col
+                new_mmd.human_readable_name = "; ".join(
+                    [
+                        f"Variable: '{key}'; Value: '{value}'"
+                        for key, value in key_val.items()
+                    ]
+                )
+                derived_mmd.append(new_mmd)
 
         joined_metrics = reduce(
             lambda left, right: left.merge(
