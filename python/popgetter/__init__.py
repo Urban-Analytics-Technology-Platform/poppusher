a58a086f7911fe71f1efc3024a3de2f537180776
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-06-06 21:13:29 +0100

    Add jobs list towards simplifying popgetter init

--- python/popgetter/__init__.py
+++ python/popgetter/__init__.py
@@ -63,30 +63,29 @@ all_assets: Sequence[AssetsDefinition | SourceAsset | CacheableAssetsDefinition]
     ),
 ]
 
-job_be: UnresolvedAssetJobDefinition = define_asset_job(
-    name="job_be",
-    selection=AssetSelection.groups("be"),
-    description="Downloads Belgian data.",
-    partitions_def=assets.be.census_tables.dataset_node_partition,
-)
-
-job_us: UnresolvedAssetJobDefinition = define_asset_job(
-    name="job_us",
-    selection=AssetSelection.groups("us"),
-    description="Downloads USA data.",
-)
-
-job_uk: UnresolvedAssetJobDefinition = define_asset_job(
-    name="job_uk",
-    selection=AssetSelection.groups("uk"),
-    description="Downloads UK data.",
-)
-
-job_ni: UnresolvedAssetJobDefinition = define_asset_job(
-    name="job_ni",
-    selection=AssetSelection.groups("ni"),
-    description="Downloads Northern Ireland data.",
-)
+jobs: list[UnresolvedAssetJobDefinition] = [
+    define_asset_job(
+        name="job_be",
+        selection=AssetSelection.groups("be"),
+        description="Downloads Belgian data.",
+        partitions_def=assets.be.census_tables.dataset_node_partition,
+    ),
+    define_asset_job(
+        name="job_us",
+        selection=AssetSelection.groups("us"),
+        description="Downloads USA data.",
+    ),
+    define_asset_job(
+        name="job_uk",
+        selection=AssetSelection.groups("uk"),
+        description="Downloads UK data.",
+    ),
+    define_asset_job(
+        name="job_ni",
+        selection=AssetSelection.groups("ni"),
+        description="Downloads Northern Ireland data.",
+    ),
+]
 
 
 def resources_by_env():
@@ -127,5 +126,5 @@ defs: Definitions = Definitions(
         cloud_outputs.metrics_sensor,
     ],
     resources=resources,
-    jobs=[job_be, job_us, job_uk, job_ni],
+    jobs=jobs,
 )
