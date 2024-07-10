334402f8260bde63048e48ddd8018111b9df2ef0
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2024-07-10 16:29:48 +0100

    fix liniting

--- python/popgetter/assets/__init__.py
+++ python/popgetter/assets/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
-
-from . import bel, gb_eaw, gb_sct, gb_nir, uk, us
+from . import bel, gb_eaw, gb_nir, gb_sct, uk, us
 
 countries = [
     (mod, mod.__name__.split(".")[-1]) for mod in [bel, gb_nir, uk, us, gb_eaw, gb_sct]
